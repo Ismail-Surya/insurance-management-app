8d8768c8bc7701f9efe37e7379562f5610e41b69
Ismail <ismail.cg373@gmail.com>
2025-12-18 21:11:14 +0530

    feat: Add a security mechanism using JWT Authorization (WIP)
    - Added tables to contain users and roles and a
      join table to contain info on what role is
      assigned to which user
    - Created model classes for roles and users.
    - Created a repo for User details which fetches
      the user info (id, username, enabled, password
      hash, and roles) from the database

---
+++ backend-app/src/main/java/in/cyclano/model/User.java
@@ -0,0 +1,49 @@
+package in.cyclano.model;
+
+import java.util.List;
+
+public class User {
+
+	private Integer id;
+	private String username;
+	private String passwordHash;
+	private boolean enabled;
+	private List <Role> roles;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public List<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", passwordHash=" + passwordHash + ", enabled=" + enabled
+				+ ", roles=" + roles + "]";
+	}
+	
+}
---
+++ backend-app/src/main/java/in/cyclano/repo/UserRepository.java
@@ -0,0 +1,58 @@
+package in.cyclano.repo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import in.cyclano.model.Role;
+import in.cyclano.model.User;
+
+@Repository
+public class UserRepository {
+
+	private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
+	public UserRepository (NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+		this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+	}
+	
+	public Optional <User> findByUsername (String username) {
+		String sql = "SELECT id, username, password_hash, enabled FROM ima_users WHERE username = :username";
+		var userList = namedParameterJdbcTemplate.query(sql,
+				new MapSqlParameterSource().addValue("username", username),
+				(myRs, rowNum) -> {
+					User u = new User();
+					u.setId(myRs.getInt("id"));
+					u.setUsername(myRs.getString("username"));
+					u.setPasswordHash(myRs.getString("password_hash"));
+					u.setEnabled(myRs.getBoolean("enabled"));
+					return u;
+				});
+		
+		if (userList.isEmpty()) {
+			return Optional.empty();
+		}
+		
+		User user = userList.get(0);
+		user.setRoles(findRolesByUserId(user.getId()));
+		return Optional.of(user);
+	}
+
+	private List<Role> findRolesByUserId(Integer userId) {
+		
+		String sql = "SELECT r.id, r.name FROM ima_roles_for_users rfu JOIN\r\n"
+				+ "    ima_roles r ON r.id = rfu.role_id\r\n"
+				+ "    WHERE rfu.user_id = :userId";
+		
+		List <Role> theRoles = namedParameterJdbcTemplate.query(sql,
+				new MapSqlParameterSource().addValue("userId", userId),
+				BeanPropertyRowMapper.newInstance(Role.class));
+		
+		return theRoles;
+	}
+	
+}
